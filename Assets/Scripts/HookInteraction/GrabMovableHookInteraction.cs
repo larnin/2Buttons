8b3050b21b5faabe1e2db65ee1dc5ecef8f8298b
larnin <nicolas-donald.laurent@laposte.net>
2019-12-10 23:18:00 +0100

    WIP movable

---
+++ Assets/Scripts/HookInteraction/GrabMovableHookInteraction.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabMovableHookInteraction : HookInteractionBase
+{
+    [SerializeField] float m_attractSpeed = 0;
+
+    public override bool Attach(GameObject hook, GameObject target)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Detach()
+    {
+        throw new System.NotImplementedException();
+    }
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
