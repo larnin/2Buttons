5c45ad407b47525aba4e5ebed2f8e67d26002278
larnin <nicolas-donald.laurent@laposte.net>
2019-12-12 23:31:06 +0100

    BOOM EXPLOSIONS !!!

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] float m_size = 1;
+    [SerializeField] float m_power = 1;
+    [SerializeField] float m_duration = 1;
+
+    float m_timer = 0;
+    
+    void Update()
+    {
+        m_timer += Time.deltaTime;
+
+        var colliders = Physics2D.OverlapCircleAll(transform.position, m_size);
+
+        Vector2 pos = transform.position;
+
+        foreach(var c in colliders)
+        {
+            if (c.gameObject == gameObject)
+                continue;
+
+            var rigidbody = c.GetComponent<Rigidbody2D>();
+            if (rigidbody == null)
+                return;
+
+            Vector2 targetPos = c.transform.position;
+
+            var dir = targetPos - pos;
+            float dist = dir.magnitude;
+            dir /= dist;
+            var orthoDir = new Vector2(dir.y, -dir.x);
+
+            float power = dist / m_size * m_power;
+
+            var velocity = rigidbody.velocity;
+
+            var dirVelocity = Utility.Project(velocity, dir);
+            var orthroVelocity = Utility.Project(velocity, orthoDir);
+
+            float distDirVelocity = dirVelocity.magnitude;
+            if (distDirVelocity < power)
+                dirVelocity = dir * power;
+
+            velocity = dirVelocity + orthroVelocity;
+
+            rigidbody.velocity = velocity;
+
+            var explosive = c.GetComponent<ExplosiveInteractable>();
+            if (explosive != null)
+                explosive.OnHit();
+        }
+
+        if (m_timer >= m_duration)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Interactable/ExplosiveInteractable.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class ExplosiveInteractable : BaseInteractable
+{
+    [SerializeField] float m_interactionExplosionDelay = 2;
+    [SerializeField] float m_hitExplosionDelay = 1;
+    [SerializeField] float m_minHitSpeedToExplode = 1;
+    [SerializeField] float m_rapidFlashDelay = 0.5f;
+    [SerializeField] GameObject m_explosionPrefab = null;
+
+    float m_timer = 0;
+    bool m_hit = false;
+
+    Animator m_animator = null;
+
+    public override void StartInteract(HookBehaviour interactor)
+    {
+        if (m_hit)
+            return;
+
+        m_timer = m_interactionExplosionDelay;
+        m_hit = true;
+    }
+
+    public override void StopInteract()
+    {
+        //nothing
+    }
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (!m_hit)
+            return;
+
+        m_timer -= Time.deltaTime;
+
+        if(m_animator != null)
+        {
+            if (m_timer < m_rapidFlashDelay)
+                m_animator.SetTrigger("SlowPulse");
+            else m_animator.SetTrigger("FastPulse");
+        }
+
+        if (m_timer <= 0)
+        {
+            OnExplode();
+            m_hit = false;
+        }
+    }
+
+    public void OnHit()
+    {
+        if (m_hit)
+            return;
+
+        m_timer = m_hitExplosionDelay;
+        m_hit = true;
+    }
+
+    void OnExplode()
+    {
+        if(m_explosionPrefab != null)
+        {
+            var obj = Instantiate(m_explosionPrefab);
+            obj.transform.position = transform.position;
+        }
+
+        DOVirtual.DelayedCall(0.1f, () => 
+        {
+            if (gameObject != null)
+                Destroy(gameObject);
+        });
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float speed = collision.relativeVelocity.magnitude;
+
+        if (speed > m_minHitSpeedToExplode)
+            OnHit();
+    }
+}
