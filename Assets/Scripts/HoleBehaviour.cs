3e5481482ec3d52e0186fd61a1aea1cd35c6cccd
larnin <nicolas-donald.laurent@laposte.net>
2019-12-14 22:56:28 +0100

    A lot of stuff

---
+++ Assets/Scripts/HoleBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var life = collision.GetComponent<LifeComponent>();
+        if (life != null)
+            life.Hit();
+    }
+}
